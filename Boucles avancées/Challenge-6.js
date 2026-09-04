8f2c1b74ba9ccdde44201fd5be4695daa63ed7c8
youssef18 <youssefodada11@gmail.com>
2026-09-04 17:17:54 +0100

    Implement Fibonacci sequence display and add binary search function

--- Boucles avancées/Challenge-6.js
+++ Boucles avancées/Challenge-6.js
@@ -2,7 +2,10 @@
 const prompt = require("prompt-sync")();
 let number = Number(prompt("Entre un numbre: "));
 let i = 1;
-while (i != number) {
-    i += i*i
-  console.log(i);
+for (let i = 1; i <= number; i++) {
+  for (let j = number; j > 0; j--) {
+    if (i * j == number) {
+      console.log(`${i} *${j} = ${number}`);
+    }
+  }
 }
--- Chaînes/Challenge-9.js
+++ Chaînes/Challenge-9.js
@@ -5,7 +5,7 @@ let phrase = prompt("Enter your desier phrase: ");
 let word = "";
 let c = 0;
 for (let i = 0; i <= phrase.length - 1; i++) {
-  if (!(phrase[i] === " ")){
+  if ((phrase[i] !== " ")){
     word += phrase[i];
   } 
 }
