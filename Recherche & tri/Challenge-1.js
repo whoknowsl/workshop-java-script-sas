282409533c177689735ae28264d34ec47455c41e
youssef18 <youssefodada11@gmail.com>
2026-09-08 16:50:11 +0100

    Refactor sorting algorithms to improve readability and maintainability by introducing size variables and optimizing loop conditions.

--- Recherche & tri/Challenge-1.js
+++ Recherche & tri/Challenge-1.js
@@ -1,11 +1,19 @@
 //Tri à Bulles
 function sortedTableau(array) {
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = 0; j < array.length - 1 - i; j++) {
+  let size = array.length - 1;
+  let swap;
+  for (let i = 0; i < size; i++) {
+    swap = false
+    for (let j = 0; j < size - i; j++) {
       if (array[j] > array[j + 1]) {
         [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        swap = true
       }
     }
+    if (!swap) {
+      break
+    }
+
   }
   return array;
 }
--- Recherche & tri/Challenge-2.js
+++ Recherche & tri/Challenge-2.js
@@ -1,8 +1,22 @@
 //Tri par Insertion
 function InsertionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-
+    let temp;
+    let size = arr.length;
+    for (let i = 0; i < size - 1; i++) {
+        let minIndx = i;
+        for (let j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndx]) {
+                minIndx = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[minIndx];
+        arr[minIndx] = temp
     }
+
+    return arr
 }
 
 console.log(InsertionSort([5, 4, 6, 1, 3, 5, 7]))
+
+
